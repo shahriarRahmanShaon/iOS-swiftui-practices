d59ced1f6fcf49199129df1ff33f84e5a09ae163
shahriarRahmanShaon <shaon463@gmail.com>
2021-03-27 20:24:01 +0600

    Initial Commit

---
+++ iOS swiftui practices/iOS_swiftui_practicesApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_swiftui_practicesApp.swift
+//  iOS swiftui practices
+//
+//  Created by sergio shaon on 27/3/21.
+//
+
+import SwiftUI
+
+@main
+struct iOS_swiftui_practicesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
